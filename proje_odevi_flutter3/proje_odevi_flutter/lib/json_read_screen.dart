3c3003d6e7cd95910c29aa79c82eaed9cda5ae75
yarendilekci <110239605+yarendilekci@users.noreply.github.com>
2024-06-10 21:48:11 +0300

    dosya_3
    Dil Desteği için kısmı ayarlamalar yapıldı, Sayfa düzenlemeleri yapıldı

---
+++ proje_odevi_flutter3/proje_odevi_flutter/lib/json_read_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JsonDataScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Json Veri Okuma'),
+      ),
+      body: Center(
+        child: Text('Json Veri Okuma Sayfası'),
+      ),
+    );
+  }
+}
---
+++ proje_odevi_flutter3/proje_odevi_flutter/lib/language_support_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguageSupportScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dil Desteği'),
+      ),
+      body: Center(
+        child: Text('Dil Desteği Sayfası'),
+      ),
+    );
+  }
+}
